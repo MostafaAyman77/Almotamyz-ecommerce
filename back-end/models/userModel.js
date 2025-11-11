365d234175f51f1f459cbfbfa9685f433bf62705
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-11-11 14:39:42 +0200

    solve bug for douple encryption password

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -62,13 +62,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  // Hashing user password
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
-});
-
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
